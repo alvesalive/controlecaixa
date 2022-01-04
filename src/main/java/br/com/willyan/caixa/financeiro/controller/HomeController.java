6a824445552b77742ca3e15868635fcb2a44fab4
Willyan Alves <willyan_af@hotmail.com>
2022-01-04 10:58:40 -0400

    change index HomeController

--- src/main/java/br/com/willyan/caixa/financeiro/controller/HomeController.java
+++ src/main/java/br/com/willyan/caixa/financeiro/controller/HomeController.java
@@ -1,20 +1,55 @@
 package br.com.willyan.caixa.financeiro.controller;
 
+import br.com.willyan.caixa.financeiro.model.Caixa;
+import br.com.willyan.caixa.financeiro.model.Movimentacao;
+import br.com.willyan.caixa.financeiro.repository.CaixaRepository;
 import br.com.willyan.caixa.financeiro.repository.MovimentacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
     @Autowired
     private MovimentacaoRepository movimentacaoRepository;
 
+    @Autowired
+    private CaixaRepository caixaRepository;
+
     @GetMapping("/")
     public String movimentacaoes(Model model){
         model.addAttribute("listaMovimentacoes", movimentacaoRepository.findAll());
+        model.addAttribute("caixas", caixaRepository.findAll());
         return "index";
     }
+
+    //TODO FIX
+//    @GetMapping("/find")
+//    public String movJan(Model model){
+//        model.addAttribute("listaMovimentacoes", movimentacaoRepository.findByData());
+//        return "index";
+//    }
+
+    @GetMapping ("/mes/{ano}")
+    public String findByAno(@PathVariable long ano, Model model){
+        //Optional<Movimentacao> movimentacaoOptional = movimentacaoRepository.findById(ano);
+//        List<Movimentacao> movimentacaoOptional = movimentacaoRepository.findAll();
+//        if (movimentacaoOptional.isEmpty()){
+//            throw new IllegalArgumentException("sem movimentação.");
+//        }
+//        model.addAttribute("movimentacao", movimentacaoOptional.add(0,);
+
+        return "index";
+
+    }
+
+
 
 }
--- src/main/java/br/com/willyan/caixa/financeiro/repository/MovimentacaoRepository.java
+++ src/main/java/br/com/willyan/caixa/financeiro/repository/MovimentacaoRepository.java
@@ -4,6 +4,13 @@ import br.com.willyan.caixa.financeiro.model.Movimentacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
+
+
+
 }
