0f6e0760cb1ee99b949ea8c262748cf4c2a1300a
Willyan Alves <willyan_af@hotmail.com>
2022-01-04 03:05:21 -0400

    create method for edit and delete movimentacao and caixa

--- src/main/java/br/com/willyan/caixa/financeiro/controller/CaixaController.java
+++ src/main/java/br/com/willyan/caixa/financeiro/controller/CaixaController.java
@@ -9,7 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 
 @Controller
@@ -28,6 +31,30 @@ public class CaixaController {
     public String novaCaixa(@ModelAttribute("caixa") Caixa caixa){
         return "caixas/formulario";
     }
+
+    @GetMapping("caixas/{id}")
+    public String alterarCaixa(@PathVariable("id") long id, Model model){
+        Optional<Caixa> caixaOptional = caixaRepository.findById(id);
+
+        if (caixaOptional.isEmpty()){
+            throw new IllegalArgumentException("conta inválida.");
+        }
+        model.addAttribute("caixa", caixaOptional.get());
+        return "caixas/formulario";
+    }
+
+    @GetMapping("/caixas/apagar/{id}")
+    public String excluirCaixa(@PathVariable("id") long id){
+        Optional<Caixa> caixaOptional = caixaRepository.findById(id);
+
+        if (caixaOptional.isEmpty()){
+            throw new IllegalArgumentException("conta inválida.");
+        }
+        caixaRepository.delete(caixaOptional.get());
+        return "redirect:/caixas";
+
+    }
+
 
     @PostMapping("caixas/salvar")
     public String salvarCaixa(@ModelAttribute("caixa") Caixa caixa){
--- src/main/java/br/com/willyan/caixa/financeiro/controller/MovimentacaoController.java
+++ src/main/java/br/com/willyan/caixa/financeiro/controller/MovimentacaoController.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 public class MovimentacaoController {
@@ -24,6 +27,29 @@ public class MovimentacaoController {
     @GetMapping("movimentacoes/nova")
     public String novaMovimentacao(@ModelAttribute("movimentacao") Movimentacao movimentacao){
         return "movimentacoes/formulario";
+    }
+
+    @GetMapping("movimentacoes/{id}")
+    public String alterarMovimentacao(@PathVariable("id") long id, Model model){
+        Optional<Movimentacao> movimentacaoOptional = movimentacaoRepository.findById(id);
+
+        if (movimentacaoOptional.isEmpty()){
+            throw new IllegalArgumentException("movimentação inválida.");
+        }
+        model.addAttribute("movimentacao", movimentacaoOptional.get(); )
+        return "movimentacoes/formulario";
+    }
+
+    @GetMapping("/movimentacoes/apagar/{id}")
+    public String excluirCaixa(@PathVariable("id") long id){
+        Optional<Movimentacao> movimentacaoOptional = movimentacaoRepository.findById(id);
+
+        if (movimentacaoOptional.isEmpty()){
+            throw new IllegalArgumentException("movimentação inválida.");
+        }
+        movimentacaoRepository.delete(movimentacaoOptional.get());
+        return "redirect:/movimentacoes";
+
     }
 
     @PostMapping("movimentacoes/salvar")
