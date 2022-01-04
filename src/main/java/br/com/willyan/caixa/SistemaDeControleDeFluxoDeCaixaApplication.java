177cce6d9f6261a7e921089bb5440b2544fcb65c
Willyan Alves <willyan_af@hotmail.com>
2022-01-04 01:11:11 -0400

    initial commit - start project Controle de Caixa

---
+++ src/main/java/br/com/willyan/caixa/SistemaDeControleDeFluxoDeCaixaApplication.java
@@ -0,0 +1,13 @@
+package br.com.willyan.caixa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeControleDeFluxoDeCaixaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeControleDeFluxoDeCaixaApplication.class, args);
+	}
+
+}
