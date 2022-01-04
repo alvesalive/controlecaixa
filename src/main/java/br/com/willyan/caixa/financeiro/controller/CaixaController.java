3e7c2ee5f425b82a0056fc69eb9da539ef23e27f
Willyan Alves <willyan_af@hotmail.com>
2022-01-04 02:50:23 -0400

    create formularios cadastro caixa e movimentação

--- src/main/java/br/com/willyan/caixa/financeiro/controller/CaixaController.java
+++ src/main/java/br/com/willyan/caixa/financeiro/controller/CaixaController.java
@@ -1,11 +1,39 @@
 package br.com.willyan.caixa.financeiro.controller;
 
 
+import br.com.willyan.caixa.financeiro.model.Caixa;
+import br.com.willyan.caixa.financeiro.repository.CaixaRepository;
+import br.com.willyan.caixa.financeiro.repository.MovimentacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class CaixaController {
+
+    @Autowired
+    private CaixaRepository caixaRepository;
+
+    @GetMapping("/caixas")
+    public String caixas(Model model){
+        model.addAttribute("listaCaixas", caixaRepository.findAll());
+        return "caixas/index";
+    }
+
+    @GetMapping("caixas/nova")
+    public String novaCaixa(@ModelAttribute("caixa") Caixa caixa){
+        return "caixas/formulario";
+    }
+
+    @PostMapping("caixas/salvar")
+    public String salvarCaixa(@ModelAttribute("caixa") Caixa caixa){
+        caixaRepository.save(caixa);
+        return "redirect:/caixas";
+    }
 
 
 }
--- src/main/java/br/com/willyan/caixa/financeiro/controller/MovimentacaoController.java
+++ src/main/java/br/com/willyan/caixa/financeiro/controller/MovimentacaoController.java
@@ -1,10 +1,14 @@
 package br.com.willyan.caixa.financeiro.controller;
 
+import br.com.willyan.caixa.financeiro.model.Caixa;
+import br.com.willyan.caixa.financeiro.model.Movimentacao;
 import br.com.willyan.caixa.financeiro.repository.MovimentacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MovimentacaoController {
@@ -16,5 +20,17 @@ public class MovimentacaoController {
         model.addAttribute("listaMovimentacoes", movimentacaoRepository.findAll());
         return "movimentacoes/index";
     }
+
+    @GetMapping("movimentacoes/nova")
+    public String novaMovimentacao(@ModelAttribute("movimentacao") Movimentacao movimentacao){
+        return "movimentacoes/formulario";
+    }
+
+    @PostMapping("movimentacoes/salvar")
+    public String salvarMovimentacao(@ModelAttribute("movimentacao") Movimentacao movimentacao){
+        movimentacaoRepository.save(movimentacao);
+        return "redirect:/movimentacoes";
+    }
+
 
 }
